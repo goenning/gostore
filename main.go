9bd1b5f16aff50a4761422a7e97bf2d2d58fb327
goenning <me@goenning.net>
2017-02-04 07:40:13 +0000

    more readable

--- main.go
+++ main.go
@@ -6,16 +6,17 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tpl, _ := template.ParseFiles("index.html")
-		data := map[string]string{
-			"Title": "Go Store :)",
-		}
-		w.WriteHeader(http.StatusOK)
-		tpl.Execute(w, data)
-	})
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl, _ := template.ParseFiles("index.html")
+	data := map[string]string{
+		"Title": "Go Store :)",
+	}
+	w.WriteHeader(http.StatusOK)
+	tpl.Execute(w, data)
+}
 
+func main() {
+	http.HandleFunc("/", index)
 	fmt.Println("Server is up and listening on port 8080.")
 	http.ListenAndServe(":8080", nil)
 }
